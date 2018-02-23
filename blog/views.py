a0b29696522d1f85253a1c50f13b5793bbe344a3
杨剑锋 <331387202@qq.com>
2018-02-24 00:19:16 +0800

    Fix bugs caused by spelling errors

--- blog/views.py
+++ blog/views.py
@@ -14,14 +14,14 @@ def index(request):
 
 
 def article_page(request, article_id):
-    article = models.Arcticle.objects.get(pk=article_id)
+    article = models.Article.objects.get(pk=article_id)
     return render(request, 'blog/article_page.html', {'article': article})
 
 
 def edit_page(request, article_id):
     if str(article_id) == '0':  # ID是从1开始的，所以新建文章时传入0
         return render(request, 'blog/edit_page.html')
-    article = models.Arcticle.objects.get(pk=article_id)
+    article = models.Article.objects.get(pk=article_id)
     return render(request, 'blog/edit_page.html', {'article': article})
 
 
@@ -31,14 +31,14 @@ def edit_action(request):
     article_id = request.POST.get('article_id', '0')
 
     if article_id == '0':
-        models.Arcticle.objects.create(title=title, content=content)
+        models.Article.objects.create(title=title, content=content)
         # articles = models.Arcticle.objects.all()
         # return render(request, 'blog/index.html', {'articles': articles})
         # 新建博客成功后会跳转到首页，这时浏览器里地址还是指向刚才的提交表单地址http://localhost:8000/blog/edit/action/
         # 这时候刷新页面，会再多添加一条数据。
         # 改成以下语句重定向
         return HttpResponseRedirect('/blog/index')
-    article = models.Arcticle.objects.get(pk=article_id)
+    article = models.Article.objects.get(pk=article_id)
     article.title = title
     article.content = content
     article.save()
