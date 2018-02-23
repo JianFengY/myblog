585b0f590e81a800555244dcf34c2f7dfe4eec85
杨剑锋 <331387202@qq.com>
2018-02-23 16:23:51 +0800

    Complete article page

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,6 @@ from . import views
 urlpatterns = [
     # url(r'^$', views.index),  # ^$分别限制开头和结尾，即限定为空
     url(r'^index/$', views.index),  # 限制index，注意加一个‘/’，否则访问“http://localhost:8000/blog/index/”会出现404
+    # 把匹配到的数字以article_id作为组名，组名必须和响应函数的参数名一致，否则会报错
+    url(r'^article/(?P<article_id>[0-9]+)/$', views.article_page, name='article_page'),
 ]
