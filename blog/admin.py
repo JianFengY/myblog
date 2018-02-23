fe382a21ec740ebf06408e589795bb3f3a6755a9
杨剑锋 <331387202@qq.com>
2018-02-24 00:01:51 +0800

    Update admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from blog.models import Article
 
+
 # Register your models here.
-admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    #属性的值是一个包含字符串格式的字段名的tuple或list。建议使用tuple，因为tuple不可变，比较安全。
+    list_display = ('title', 'content', 'pub_time')
+    list_filter = ('pub_time',)  # tuple只有一个元素时记得加个逗号
+
+
+admin.site.register(Article, ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=32, default='Title')  # 必选参数max_length
     content = models.TextField(null=True)  # 没有必选参数
+    # pub_time = models.DateTimeField(auto_now=True)  # auto_now意思是创建对象时自动设置为当前时间
+    pub_time = models.DateTimeField(null=True)
 
     def __str__(self):
         """Python语法，用于显示这个类的实例对象打印出来的效果"""
